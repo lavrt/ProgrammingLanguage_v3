2b77d8a754b88f1124e1137131801318edcf24ae
Alexey <lavrik-aleksey-1@yandex.ru>
2026-03-10 02:52:32 +0300

    fix codestyle in proc.cpp

--- src/app/proc/proc.cpp
+++ src/app/proc/proc.cpp
@@ -16,29 +16,19 @@ namespace bfs = boost::filesystem;
 
 ProcessResult RunProcess(const bfs::path& exe, const std::vector<std::string>& args) {
     asio::io_context ctx;
-    asio::readable_pipe err_pipe{ctx};
+    asio::readable_pipe stderr_pipe{ctx};
 
-    bp::process proc{
-        ctx,
-        exe,
-        args,
-        bp::process_stdio{
-            {},
-            {},
-            err_pipe
-        }
-    };
+    bp::process proc{ctx, exe, args, bp::process_stdio{{}, {}, stderr_pipe}};
 
     std::string stderr_text;
     boost::system::error_code ec;
-
-    asio::read(err_pipe, asio::dynamic_buffer(stderr_text), ec);
+    asio::read(stderr_pipe, asio::dynamic_buffer(stderr_text), ec);
 
     proc.wait();
 
     return {
-        proc.exit_code(),
-        std::move(stderr_text)
+        .exit_code = proc.exit_code(),
+        .stderr_text = std::move(stderr_text)
     };
 }
 
