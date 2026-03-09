4faa7124f486077306a0e2a238f2ade4660d50de
Alexey <lavrik-aleksey-1@yandex.ru>
2026-03-09 20:38:40 +0300

    split the code into files more accurate

---
+++ common/tokens.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+
+inline const std::string keyIf = "if";
+inline const std::string keyAdd = "+";
+inline const std::string keySub = "-";
+inline const std::string keyMul = "*";
+inline const std::string keyDiv = "/";
+inline const std::string keyLess = "<";
+inline const std::string keyDef = "def";
+inline const std::string keyEnd = "end";
+inline const std::string keyEqual = "=";
+inline const std::string keyGreater = ">";
+inline const std::string keyCall = "call";
+inline const std::string keyPrintAscii = "print_ascii";
+inline const std::string keyPrintInt = "print_int";
+inline const std::string keyReadInt = "read_int";
+inline const std::string keyWhile = "while";
+inline const std::string keySemicolon = ";";
+inline const std::string keyIdentical = "==";
+inline const std::string keyReturn = "return";
+inline const std::string keyLessOrEqual = "<=";
+inline const std::string keyNotIdentical = "!=";
+inline const std::string keyGreaterOrEqual = ">=";
+inline const std::string keyLeftParenthesis = "(";
+inline const std::string keyRightParenthesis = ")";
+inline const std::string keyLeftCurlyBracket = "{";
+inline const std::string keyRightCurlyBracket = "}";
+inline const std::string keyNumber = "number";
+inline const std::string keyIdentifier = "identifier";
+inline const std::string keyOperation = "operation";
+inline const std::string keyComma = ",";
---
+++ tree/include/node.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <string>
+#include <memory>
+
+#include "node_types.hpp"
+
+class Node {
+private:
+    NodeType type;
+    std::string value;
+    std::unique_ptr<Node> left;
+    std::unique_ptr<Node> right;
+
+public:
+    Node(NodeType t, const std::string& v, std::unique_ptr<Node> l, std::unique_ptr<Node> r)
+        : type(t), value(v), left(std::move(l)), right(std::move(r)) {}
+
+    Node(NodeType t, const std::string& v)
+        : type(t), value(v), left(nullptr), right(nullptr) {}
+
+    Node() : left(nullptr), right(nullptr) {}
+
+    void SetType(NodeType t) {
+        type = t;
+    }
+
+    void SetValue(const std::string& v) {
+        value = v;
+    }
+
+    void SetLeft(std::unique_ptr<Node> l) {
+        left = std::move(l);
+    }
+
+    void SetRight(std::unique_ptr<Node> r) {
+        right = std::move(r);
+    }
+
+    NodeType GetType() const noexcept {
+        return type;
+    }
+
+    std::string GetValue() const noexcept {
+        return value;
+    }
+
+    Node* GetLeft() const noexcept {
+        return left.get();
+    }
+
+    Node* GetRight() const noexcept {
+        return right.get();
+    }
+};
